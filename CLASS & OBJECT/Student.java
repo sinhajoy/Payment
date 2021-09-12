1b207aaa0d87c25ff36e01f5b5c5921d7c4b551c
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-09-12 17:15:32 +0530

    commit

--- CLASS & OBJECT/Student.java
+++ CLASS & OBJECT/Student.java
@@ -10,12 +10,37 @@ public class Student {
         this.address = address;
     }
 
-    
-    
+    public void setName(String name)
+    {
+        this.name=name;
+    }
 
+    public String getName(String name)
+    { 
+        return name;
+    }
 
+    public void setAge(int age)
+    {
+        this.age=age;
+    }
 
+    public int getAge(int age)
+    {
+        return age;
+    }
 
+    public void setAddress(String address){
+        this.address=address;
+    }
 
+    public String getAddress(String address){
+        return address;
+    }
 
+    @Override
+    public String toString()
+    {
+        return ("Student name is "+this.name+". Student age is "+this.age+" and address is "+this.address);
+    }
 }
---
+++ CLASS & OBJECT/TestStudent.java
@@ -0,0 +1,6 @@
+public class TestStudent {
+    public static void main(String[] args) {
+        Student joy=new Student("Joy",18,"Budge budge");
+        System.out.println(joy.toString());
+    }
+}
